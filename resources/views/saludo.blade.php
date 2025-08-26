1b062706ec92ae6ad75f34f4592a3b3bef3fd34d
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-08-25 20:57:57 -0500

    Añadida ruta de saludo y preparado para despliegue

---
+++ resources/views/saludo.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Saludo</title>
+</head>
+<body>
+    <h1>¡🌸HOLA SOY YUDITH PACCO 🌸!</h1>
+    <p>🌸🌸¡ESTE ES EL LABORATORIO 02!🌸🌸</p>
+</body>
+</html>
